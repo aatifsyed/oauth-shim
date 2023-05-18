6c5595238460480f69d4a526a928f7b979f640e8
Aatif Syed <aatifsyedyp@gmail.com>
2023-05-18 19:19:27 +0100

    feat: add styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -59,26 +59,48 @@
 </script>
 
 {#if result.type == 'ok'}
-	<h1>Success</h1>
-	<p>Copy using the button below, and paste into the app that you're authorizing.</p>
-	<p>You can then close this page.</p>
-	<button on:click={oauth_to_clipboard}>Copy authorization to clipboard</button>
+	<main>
+		<h1>Success</h1>
+		<p>Copy using the button below, and paste into the app that you're authorizing.</p>
+		<p>You can then close this page.</p>
+		<button on:click={oauth_to_clipboard}>Copy authorization to clipboard</button>
+	</main>
 {:else if result.type == 'err'}
-	<h1>Authorization error</h1>
-	<p>error: {result.error}</p>
-	{#each [['description', result.description], ['uri', result.uri], ['state', result.state]] as [name, maybe_item]}
-		{#if maybe_item != undefined}
-			<p>{name}: {maybe_item}</p>
-		{/if}
-	{/each}
+	<main style="--color-accent: #cc00aa44">
+		<h1>Authorization error :(</h1>
+		<p>Please report a bug to the app developer that sent you here.</p>
+		<p>Here is additional information about the error:</p>
+		<br />
+		<table>
+			<tr>
+				<th>parameter</th>
+				<th>value</th>
+			</tr>
+			<tr>
+				<td><code>error</code></td>
+				<td><code>{result.error}</code></td>
+			</tr>
+			{#each [['description', result.description], ['uri', result.uri], ['state', result.state]] as [name, maybe_item]}
+				{#if maybe_item != undefined}
+					<tr>
+						<td><code>{name}</code></td>
+						<td><code>{maybe_item}</code></td>
+					</tr>
+				{/if}
+			{/each}
+		</table>
+	</main>
 {:else}
-	<h1>OAuth Shim</h1>
-	<p>A simple redirect URL for OAuth applications.</p>
-	<p>
-		When used as a redirect, this page will present a button which copies the OAuth <code>code</code
-		>
-		and <code> state </code> (if present) to the user's clipboard.
-	</p>
-	<p>These will be copied as JSON, which the user can then paste into your application.</p>
-	<p>On error, the params in the OAuth spec are printed out, if present.</p>
+	<main>
+		<h1>OAuth Shim</h1>
+		<p>A simple redirect URL for OAuth applications.</p>
+		<p>
+			When used as a redirect, this page will present a button which copies the OAuth <code
+				>code</code
+			>
+			and <code> state </code> (if present) to the user's clipboard.
+		</p>
+		<p>These will be copied as JSON, which the user can then paste into your application.</p>
+		<p>On error, the params in the OAuth spec are printed out, if present.</p>
+	</main>
 {/if}
