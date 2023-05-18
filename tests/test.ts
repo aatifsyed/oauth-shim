3cfe1c021f0954da8ef56321e37e3307c38a50c4
Aatif Syed <aatifsyedyp@gmail.com>
2023-05-18 18:42:52 +0100

    test: add tests

--- tests/test.ts
+++ tests/test.ts
@@ -1,6 +1,28 @@
-import { expect, test } from '@playwright/test';
+import { expect, test, type Page } from '@playwright/test';
 
-test('index page has expected h1', async ({ page }) => {
+
+test('no params has website description', async ({ page }) => {
 	await page.goto('/');
-	await expect(page.getByRole('heading', { name: 'Welcome to SvelteKit' })).toBeVisible();
+	await expect(page.getByText("A simple redirect URL for OAuth applications")).toBeVisible();
 });
+
+test('code param is copied', async ({ page }) => {
+	await page.goto("/?code=foo");
+	await page.getByRole("button").click();
+
+	let text = await page.evaluate("navigator.clipboard.readText()");
+	expect(text).toContain('{"code":"foo"}');
+})
+
+test('code and state params are copied', async ({ page }) => {
+	await page.goto("/?code=foo&state=bar");
+	await page.getByRole("button").click();
+
+	let text = await page.evaluate("navigator.clipboard.readText()");
+	expect(text).toContain('{"code":"foo","state":"bar"}');
+})
+
+test('error has a heading', async ({ page }) => {
+	await page.goto("/?error=foo");
+	await expect(page.getByRole("heading", { name: "Authorization Error" })).toBeVisible();
+})
